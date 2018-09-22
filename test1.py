d635b0e51c2ae4df506a256e53392655450e59bd
Pete Martin <pete@pixability.com>
2018-09-22 09:53:36 -0400

    Added the notebook and added showing examples of false predictions.

--- test1.py
+++ test1.py
@@ -1,4 +1,5 @@
 import csv
+import datetime
 import os
 import random
 
@@ -17,8 +18,7 @@ from tqdm import tqdm
 random.seed(34283428)
 base_dir_path = os.path.dirname(os.path.realpath(__file__))
 best_model_filepath = '{}/saved-models/model1.hdf5'.format(base_dir_path)
-training_epochs = 10
-
+training_epochs = 1
 
 def path_to_tensor(img_path):
     # loads RGB image as PIL.Image.Image type
@@ -33,6 +33,8 @@ def paths_to_tensor(img_paths):
     list_of_tensors = [path_to_tensor(img_path) for img_path in tqdm(img_paths)]
     return np.vstack(list_of_tensors)
 
+start_time = datetime.datetime.utcnow()
+print(".. loading & splitting data ..")
 
 image_to_label = {}
 images_list = []
@@ -66,6 +68,10 @@ train_tensors = paths_to_tensor(X_train).astype('float32')
 test_tensors = paths_to_tensor(X_test).astype('float32')
 valid_tensors = paths_to_tensor(X_validate).astype('float32')
 
+loading_finished_time = datetime.datetime.utcnow()
+duration_loading = (loading_finished_time - start_time).total_seconds()
+print(".. constructing the model ..")
+
 model = Sequential()
 
 model.add(Conv2D(filters=16, kernel_size=2, padding='same', input_shape=(224, 224, 3)))
@@ -90,6 +96,8 @@ model.summary()
 
 model.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['accuracy'])
 
+print(".. training the model ..")
+
 checkpointer = ModelCheckpoint(filepath=best_model_filepath,
                                verbose=1,
                                save_best_only=True)
@@ -101,16 +109,29 @@ model.fit(train_tensors, y_train,
           callbacks=[checkpointer],
           verbose=1)
 
+training_finished_time = datetime.datetime.utcnow()
+duration_training = (training_finished_time - loading_finished_time).total_seconds()
+
+print(".. loading best weights ..")
+
 model.load_weights(best_model_filepath)
+
+print(".. testing the model ..")
 
 tmp_predictions = [model.predict(np.expand_dims(tensor, axis=0)) for tensor in test_tensors]
 test_predictions = [np.argmax(model.predict(np.expand_dims(tensor, axis=0))) for tensor in test_tensors]
+
+testing_finished_time = datetime.datetime.utcnow()
+duration_testing = (testing_finished_time - training_finished_time).total_seconds()
+
+print(".. reporting results ..")
 
 true_positives = 0
 false_positives = 0
 true_negatives = 0
 false_negatives = 0
 all_positives = 0
+all = len(X_test)
 
 for n, test_image in enumerate(X_test):
     prediction_label = True if test_predictions[n] else False
@@ -139,11 +160,16 @@ for n, test_image in enumerate(X_test):
         else:
             true_negatives += 1
 
-print("all: ", len(X_test))
+print("loading duration: {0:.1f} seconds".format(duration_loading))
+print("training duration: {0:.1f} seconds".format(duration_training))
+print("testing duration: {0:.1f} seconds".format(duration_testing))
+
+print("all: ", all)
 print("all_positives: ", all_positives)
 print("true_positives: ", true_positives)
 print("true_negatives: ", true_negatives)
 print("false_positives: ", false_positives)
-print("false_negatives: ", true_negatives)
+print("false_negatives: ", false_negatives)
 
-print("RECALL: ", true_positives / all_positives)
+print("RECALL: {0:.2f}%".format(100 * true_positives / all_positives))
+print("ACCURACY: {0:.2f}%".format(100 * (true_positives + true_negatives) / all))
